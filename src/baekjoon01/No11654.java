032e2b7d7c0f404b4b3f16c8801e9e6800f14432
thirdK <dla0625@gmail.com>
2022-03-05 14:02:42 +0900

    Create No11654.java

---
+++ src/baekjoon01/No11654.java
@@ -0,0 +1,21 @@
+package baekjoon01;
+
+import java.io.IOException;
+
+//문자열 문제
+//알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 
+//주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+//입력
+//알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+
+//출력
+//입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+
+public class No11654 {
+	public static void main(String[] args) throws IOException{
+		 System.out.println("aa");
+		 int c = System.in.read();	//입력받은 문자를 아스키코드로 반환
+		 System.out.println(c);
+	}
+}
