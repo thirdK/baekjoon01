3ab682134959a80245f0ebf233358d0187f9a291
thirdK <dla0625@gmail.com>
2022-03-13 13:28:00 +0900

    Create No2447.java

---
+++ src/baekjoon01/No2447.java
@@ -0,0 +1,56 @@
+package baekjoon01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//문제
+//재귀적인 패턴으로 별을 찍어 보자. N이 3의 거듭제곱(3, 9, 27, ...)이라고 할 때, 
+//크기 N의 패턴은 N×N 정사각형 모양이다.
+//
+//크기 3의 패턴은 가운데에 공백이 있고, 가운데를 제외한 모든 칸에 별이 하나씩 있는 패턴이다.
+//
+//***
+//* *
+//***
+
+//N이 3보다 클 경우, 크기 N의 패턴은 공백으로 채워진 가운데의 (N/3)×(N/3) 
+//정사각형을 크기 N/3의 패턴으로 둘러싼 형태이다. 예를 들어 크기 27의 패턴은 예제 출력 1과 같다.
+//
+//입력
+//첫째 줄에 N이 주어진다. N은 3의 거듭제곱이다. 즉 어떤 정수 k에 대해 N=3k이며, 이때 1 ≤ k < 8이다.
+//
+//출력
+//첫째 줄부터 N번째 줄까지 별을 출력한다.
+
+public class No2447 {
+
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		System.out.println('a');
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				printStar(i, j);
+			}
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+	}
+
+	static void printStar(int x, int y) {
+		if (x < 3 && y < 3) {
+			if (x % 3 == 1 && y % 3 == 1) sb.append(' ');
+			else sb.append('*');
+		} else {
+			if (x % 3 == 1 && y % 3 == 1) sb.append(' ');
+			else printStar(x / 3, y / 3);
+		}
+	}
+
+}
