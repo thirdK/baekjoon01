c6bc6200e91e6c1ecc9c3fc9c0c3b6c8f0af6f96
thirdK <dla0625@gmail.com>
2022-05-13 22:47:05 +0900

    a

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,32 +1,55 @@
 package test;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class Test {
-	public static void main(String[] args) throws IOException {
-		System.out.println("d");
+	public static void main(String[] args) throws IOException{
+		System.out.println('a');
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int numSub = Integer.parseInt(br.readLine());
-        int[] scores= new int[numSub];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<numSub; i++){
-            scores[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(scores);
-        int total=0;
-        for(int i=0; i<numSub; i++){
-            total += scores[i]*100.0/scores[numSub-1];
-        }
-        sb.append((double)total/numSub);
-        System.out.print(sb);
+		StringBuilder sb = new StringBuilder();
+		int len = Integer.parseInt(br.readLine());
+		int[] arr = new int[10001];
+		
+		for(int i=0; i<len; i++) {
+			arr[Integer.parseInt(br.readLine())]++;
+		}
+		
+//		for(int i : countingSort(arr)) {
+//			sb.append(i + "\n");
+//		}
+		
+		for(int i=1; i<10001; i++) {
+			while(arr[i] > 0) {
+				sb.append(i).append('\n');
+				arr[i]--;
+			}
+		}
+		
+		
+		System.out.print(sb);
+		
 	}
+	
+//	static int[] countingSort(int[] arr) {
+//		int[] counting = new int[10001];
+//		int[] result = new int[arr.length];
+//		
+//		for(int i=0; i < arr.length; i++) {
+//			counting[arr[i]]++;
+//		}
+//		
+//		for(int i=1; i<counting.length; i++) {
+//			counting[i] += counting[i-1];
+//		}
+//		
+//		for(int i=0; i<arr.length; i++) {
+//			result[--counting[arr[i]]] = arr[i];
+//		}
+//		
+//		return result;
+//	}
 }
